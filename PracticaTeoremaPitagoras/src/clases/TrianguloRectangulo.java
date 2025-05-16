84e466906c3fa13ee7dc83e5c541a8f881236bdd
SalvaG <prog07x@gmail.com>
2025-05-16 09:10:56 +0200

    practica

---
+++ PracticaTeoremaPitagoras/src/clases/TrianguloRectangulo.java
@@ -0,0 +1,17 @@
+package clases;
+
+public class TrianguloRectangulo {
+	
+	int a , b , c;
+	
+	public TrianguloRectangulo() {
+		this.a = 1;
+		this.b = 1;
+	}
+	
+	public TrianguloRectangulo(int a , int b) {
+		this.a = a;
+		this.b = b;
+	}
+
+}
